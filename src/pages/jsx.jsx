6c4cd3b5cfc44bdf1551ad2a556215475df374b4
eduardo7321 <eduardo.jose184@gmail.com>
2021-09-30 23:42:13 -0300

    Componente navegador

--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -1,3 +1,4 @@
+import Layout from "../components/Layout"
 export default function Jsx() {
     const titulo = <h1>JSX é um conceito Central</h1>
 
@@ -5,12 +6,14 @@ export default function Jsx() {
         return <h2>{"muito legal".toUpperCase()}</h2>
     }
     return (
+        <Layout titulo="Entendendo o JSX">
         <div>
-            {titulo}
+        {titulo}
             {subtitulo()}
             <p>
                 {JSON.stringify({nome: 'João', idade: 30})}
             </p>
         </div>
+        </Layout>
     )
 }
