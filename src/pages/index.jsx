d5e4c9152c9bc3f48b2d990f00c158344bd3fecd
eduardo7321 <eduardo.jose184@gmail.com>
2021-09-30 22:42:32 -0300

    aula 439 - 6:16

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,15 @@
-import Link from 'next/link'
+import Navegador from '../components/Navegador'
 
 export default function Inicio() {
     return (
-        <div>
-            <Link href="/estiloso">
-                Estiloso
-            </Link>
-           
+        <div style={{
+            display:'flex',
+            
+        }}>
+            <Navegador texto="Estiloso" destino="/estiloso" />           
+            <Navegador texto="Exemplo" destino="/exemplo" />           
+            <Navegador texto="JSX" destino="/jsx" />           
+                    
         </div>
     )
 }
