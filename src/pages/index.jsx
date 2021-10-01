e353e23271d8b7a9defd92b56395f3cf51f97876
Eduardo.silva <eduardo.jose184@gmail.com>
2021-10-01 13:28:55 -0300

    Componente com estado

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,6 +15,7 @@ export default function Inicio() {
             <Navegador texto="JSX" destino="/jsx" cor="crimson" />           
             <Navegador texto="Navegação #01" destino="/navegacao/" cor="green" />           
             <Navegador texto="Navegação #02" destino="/cliente/pe-2/123" cor="blue" />           
+            <Navegador texto="Navegação componente estado #03" destino="/estado" cor="pink" />           
                     
         </div>
     )
